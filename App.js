544d3417e3bc80ce6f6f60b9cd1b58e8b4e72c33
nguyensonhai <nguyensonhai1009@gmail.com>
2020-04-26 17:37:06 +0700

    update

--- App.js
+++ App.js
@@ -1,12 +1,28 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TouchableOpacity } from 'react-native';
+import { AntDesign } from '@expo/vector-icons';
+import colors from './shared/colors';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <View style={{ flexDirection: 'row' }}>
+          <View style={styles.divider} />
+          <Text style={styles.title}>
+            Todo <Text style={{ fontWeight: '300', color: colors.green }}>Lists</Text>
+          </Text>
+          <View style={styles.divider} />
+        </View>
+        <View style={{ marginVertical: 48 }}>
+          <TouchableOpacity style={styles.addList}>
+            <AntDesign name='plus' size={16} color={colors.green} />
+          </TouchableOpacity>
+          <Text style={styles.add}>Add list</Text>
+        </View>
+      </View>
+    )
+  }
 }
 
 const styles = StyleSheet.create({
@@ -16,4 +32,30 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  divider: {
+    backgroundColor: colors.lightGreen,
+    height: 1,
+    flex: 1,
+    alignSelf: 'center',
+  },
+  title: {
+    fontSize: 38,
+    fontWeight: '800',
+    color: colors.black,
+    paddingHorizontal: 64,
+  },
+  addList: {
+    borderWidth: 2,
+    borderColor: colors.lightGreen,
+    borderRadius: 4,
+    padding: 16,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  add: {
+    color: colors.green,
+    fontWeight: '600',
+    fontSize: 14,
+    marginTop: 8,
+  }
 });
---
+++ shared/colors.js
@@ -0,0 +1,6 @@
+export default colors = {
+    black:'#333',
+    green: '#86E3CE',
+    lightGreen: '#81dbc7',
+    white: '#FFFFFF',
+};
