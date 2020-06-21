f24e0bb1e3ac2302dd8a4eeffa1688c3da27d703
nguyensonhai <nguyensonhai1009@gmail.com>
2020-06-22 00:49:51 +0700

    update

--- shared/Fire.js
+++ shared/Fire.js
@@ -1,64 +1,64 @@
-import firebase from 'firebase';
-import '@firebase/firestore';
+import firebase from "firebase";
+import "@firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCO6pFFe5r3twkfOlVsuFg8WfOkURzbfgw",
-    authDomain: "eztodo-a37d0.firebaseapp.com",
-    databaseURL: "https://eztodo-a37d0.firebaseio.com",
-    projectId: "eztodo-a37d0",
-    storageBucket: "eztodo-a37d0.appspot.com",
-    messagingSenderId: "1090588679539",
-    appId: "1:1090588679539:web:d85e71afefcdcfc5bff906",
-    measurementId: "G-WKV2GJM5XT"
+  apiKey: "AIzaSyCO6pFFe5r3twkfOlVsuFg8WfOkURzbfgw",
+  authDomain: "eztodo-a37d0.firebaseapp.com",
+  databaseURL: "https://eztodo-a37d0.firebaseio.com",
+  projectId: "eztodo-a37d0",
+  storageBucket: "eztodo-a37d0.appspot.com",
+  messagingSenderId: "1090588679539",
+  appId: "1:1090588679539:web:d85e71afefcdcfc5bff906",
+  measurementId: "G-WKV2GJM5XT",
 };
 
 class Fire {
-    constructor(callback) {
-        this.init(callback);
-    }
-    init(callback) {
-        if (!firebase.apps.length) {
-            firebase.initializeApp(firebaseConfig)
-        }
-
-        firebase.auth().onAuthStateChanged(user => {
-            if (user) {
-                callback(null, user);
-            } else {
-                firebase
-                    .auth()
-                    .signInAnonymously()
-                    .catch(error => {
-                        callback(error)
-                    })
-            }
-        })
+  constructor(callback) {
+    this.init(callback);
+  }
+  init(callback) {
+    if (!firebase.apps.length) {
+      firebase.initializeApp(firebaseConfig);
     }
 
-    getLists(callback) {
-        let ref = firebase
-            .firestore()
-            .collection('users')
-            .doc(this.userID)
-            .collection('lists');
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        callback(null, user);
+      } else {
+        firebase
+          .auth()
+          .signInAnonymously()
+          .catch((error) => {
+            callback(error);
+          });
+      }
+    });
+  }
 
-        this.unsubscribe = ref.onSnapshot(snapshot => {
-            lists = [];
+  getLists(callback) {
+    let ref = firebase
+      .firestore()
+      .collection("users")
+      .doc(this.userID)
+      .collection("lists");
 
-            snapshot.forEach(doc => {
-                lists.push({ id: doc.id, ...doc.data() });
-            });
-            callback(lists);
-        });
-    }
+    this.unsubscribe = ref.onSnapshot((snapshot) => {
+      lists = [];
 
-    get userID() {
-        return firebase.auth().currentUser.uid
-    }
+      snapshot.forEach((doc) => {
+        lists.push({ id: doc.id, ...doc.data() });
+      });
+      callback(lists);
+    });
+  }
 
-    detach() {
-        this.unsubscribe();
-    }
+  get userID() {
+    return firebase.auth().currentUser.uid;
+  }
+
+  detach() {
+    this.unsubscribe();
+  }
 }
 
 export default Fire;
--- shared/colors.js
+++ shared/colors.js
@@ -1,8 +1,8 @@
 export default colors = {
-    black:'#333',
-    gray: '#A4A4A4',
-    lightGray: '#CACACA',
-    green: '#86E3CE',
-    lightGreen: '#81dbc7',
-    white: '#FFFFFF',
+  black: "#333",
+  gray: "#A4A4A4",
+  lightGray: "#CACACA",
+  green: "#86E3CE",
+  lightGreen: "#81dbc7",
+  white: "#FFFFFF",
 };
